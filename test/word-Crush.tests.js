3b72aff2ae1d7b072439433f253426307ce9c0f2
Given_Ledwaba <99291523+Given-coding@users.noreply.github.com>
2022-08-29 11:51:09 +0200

    added files

---
+++ test/word-Crush.tests.js
@@ -0,0 +1,20 @@
+import assert from 'assert';
+import greet from '../word-Crush.js';
+
+describe('Testing the Word-Crush function' , function(){
+    it('the moment you enter the sentence, it shoult return the longest word' , function(){
+       assert.equal(2,2);
+
+        assert.deepEqual([2,2],[2,2]);
+    });
+    it('the moment you enter the sentance, it should return the shortest word' , function(){
+       assert.equal(2,2);
+
+        assert.deepEqual([2,2],[2,2]);
+    });
+    it('the moment you enter the sentance, it should return the number/sum of the sentance' , function(){
+        assert.equal(2,2);
+ 
+         assert.deepEqual([2,2],[2,2]);
+     });
+});
